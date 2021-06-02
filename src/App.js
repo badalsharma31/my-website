d80811faa7c6fff3d59e4acd725c2ecc11829386
badal <badalsharma31@gmail.com>
2021-06-02 21:24:31 +0530

    initail repo

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 }
 
